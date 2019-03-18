21e45f43800e95febfa1b6a6a415a4b9b1f4a4af
Ryun Shofner <ryan@humboldtweb.com>
2019-03-18 10:39:58 -0700

    fixes badge status

--- src/Presenters/PagePresenter.php
+++ src/Presenters/PagePresenter.php
@@ -43,7 +43,7 @@ class PagePresenter
         if ( ! $tree->isEmpty()) {
             foreach ($tree as $node) {
                 $output .= '<li class="dd-item" data-id="'.$node->id.'">'.'<div class="dd-handle">&nbsp;</div>'.'<div class="dd-content">'.$node->title;
-                $output .= ($node->published == 0) ? '<span class="label round">draft</span>' : '';
+                $output .= ($node->published) ? '' : ' <span class="badge badge-pill badge-secondary">draft</span>';
                 $output .= '<div class="actions">'.'<div class="btn-group">'.'<a href="/admin/pages/info/'.$node->id.'" class="tip" title="Info"><i class="fa fa-info"></i></a>'.'<a href="'.route('get.admin.pages.create',
                         [$node->id]).'" class="tip" title="Add child page"><i class="fa fa-plus"></i></a>'.'<a href="'.route('get.admin.pages.edit',
                         [$node->id]).'" class="tip" title="Edit"><i class="fa fa-edit"></i></a>'.'<a href="'.route('get.admin.pages.delete',
